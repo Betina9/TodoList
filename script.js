06e3642e4740aeb1a6d8b847592ad7cf6fd6cc6b
Betina9 <ob042025@gmail.com>
2025-07-01 13:37:10 +0200

    lagt inn masse

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const listContainer = document.querySelector("#list-container");
+const taskInput = document.querySelector('#task-input');
+const toggleShowCompleted = document.querySelector("#show-completed");
+const sortBy = document.querySelector("#sort-by");
 
 let tasks = [];
-let filters = { showCompleted: false };
+let filters = { showCompleted: false, sortType: "time-desc" };
+
+const saveTasksToStorage = () =>
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+sortBy.addEventListener("change", (e) => {
+  filters.sortType = e.target.value;
+  renderPage();
+});
+
+toggleShowCompleted.addEventListener("change", (e) => {
+  filters.showCompleted = e.target.checked;
+  renderPage();
+});
 
 const taskForm = document.querySelector("#task-form");
 taskForm.addEventListener("submit", (e) => {
@@ -14,11 +30,14 @@ taskForm.addEventListener("submit", (e) => {
     return alert("Input cannot be empty.");
   }
 
+taskInput.value = "";
+
   tasks.push({
     timestamp: new Date(),
     description: userInput,
     completed: false,
   });
+  saveTasksToStorage();
 
   renderPage();
 });
@@ -30,15 +49,67 @@ const completeTaskInput = (task) => {
 
   inputElement.addEventListener("change", (e) => {
     task.completed = e.target.checked;
+    saveTasksToStorage();
     renderPage();
   });
 
   return inputElement;
 };
 
-const filterArray = (tasksArr) => {
-  return tasksArr.filter((task) => filters.showCompleted || !task.completed);
+const editTaskButton = (task, descriptionElement) => {
+  const buttonElement = document.createElement("button");
+  buttonElement.classList.add("edit-button");
+  buttonElement.textContent = "Endre";
+
+  buttonElement.addEventListener("click", () => {
+    task.description = descriptionElement.value;
+    descriptionElement.readOnly = !descriptionElement.readOnly;
+    buttonElement.textContent = descriptionElement.readOnly ? "Endre" : "Lagre";
+    saveTasksToStorage();
+  });
+
+  return buttonElement;
 };
+
+const deleteTaskButton = (task) => {
+  const buttonElement = document.createElement("button");
+  buttonElement.classList.add("delete-button");
+  buttonElement.textContent = "Slett";
+
+  buttonElement.addEventListener("click", () => {
+    const taskIndex = tasks.indexOf(task);
+    if (taskIndex > -1) {
+      tasks.splice(taskIndex, 1);
+    }
+    saveTasksToStorage();
+    renderPage();
+  });
+
+  return buttonElement;
+};
+
+const filterArray = (tasksArr) => {
+  return tasksArr
+  .filter((task) => filters.showCompleted || !task.completed)
+  .sort(sortArray);
+};
+
+const sortArray = (a, b) => {
+if (filters.sortType === 'time-asc') {
+  return new Date(b.timestamp) - new Date(a.timestamp
+  )
+}
+else if (filters.sortType === 'time-desc')
+ return new Date(a.timestamp) - new Date(b.timestamp)
+}
+
+else if (filters.sortType === 'alpha-desc') {
+  return a.description.localeCompare(b.description);
+}
+
+else if (filters.sortType === 'alpha-asc') {
+  return b.description.localeCompare(a.description);
+}
 
 const buildPage = (tasksArr) => {
   console.log(tasksArr);
@@ -51,18 +122,32 @@ const buildPage = (tasksArr) => {
     timestampElement.classList.add("datetime");
     timestampElement.textContent = task.timestamp;
 
-    const descriptionElement = document.createElement("p");
+    const descriptionElement = document.createElement("input");
     descriptionElement.classList.add("description");
-    descriptionElement.textContent = task.description;
+    descriptionElement.readOnly = true;
+    descriptionElement.value = task.description;
 
     // Buttons
     const inputElement = completeTaskInput(task);
+    const editBtn = editTaskButton(task, descriptionElement);
+    const deleteBtn = deleteTaskButton(task);
 
-    taskContainer.append(timestampElement, descriptionElement, inputElement);
+    taskContainer.append(
+      timestampElement,
+      descriptionElement,
+      inputElement,
+      editBtn,
+      deleteBtn
+    );
+
     listContainer.prepend(taskContainer);
   });
 };
 
 const renderPage = () => {
+  const storedTasks = localStorage.getItem("tasks");
+  if (storedTasks) {
+    tasks = JSON.parse(storedTasks);
+  }
   buildPage(filterArray(tasks));
 };
